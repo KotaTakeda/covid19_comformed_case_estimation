b6e1346ff3d15e8bfd8ae312e1c260eae31f0cfa
Kota Takeda <takeda_kl@icloud.com>
2020-08-27 21:53:43 +0900

    commit

--- module/kalman_filters.py
+++ module/kalman_filters.py
@@ -69,7 +69,7 @@ class EnKalmanFilter:
         e = multivariate_normal(self.mean_y, self.R, N)
         ensemble_y = np.zeros((N, dim_y))
         for i in range(N):
-            ensemble_y[i] = self.H(self.ensemble[i]) + e[i]
+            ensemble_y[i] = self.H(self.ensemble[i]) + e[i] # Hが非線型でも計算できる．
 
         y_mean = np.mean(ensemble_y, axis=0)
 
@@ -78,9 +78,9 @@ class EnKalmanFilter:
         dY = ensemble_y - y_mean
 
         # Pxy Pyy
-        P_xy = (dX.T@dY) / (N-1)
-        P_yy = (dY.T@dY) / (N-1) 
-
+        P_xy = (dX.T@dY) / (N-1) # E (HE)^T= P_f Hに対応 (P_f = E E^T)
+        P_yy = (dY.T@dY) / (N-1)  # (HE)^T HE = HP_fHに対応
+        
         # Kalman gain 
         K = P_xy@np.linalg.inv(P_yy + self.R)
 
---
+++ module/util.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def mean_in_last_week(arr):
+    mean = []
+    for i in range(len(arr)-6):
+        mean.append(arr[i:i+7].mean())
+    return np.array(mean)
